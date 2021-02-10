1d98a7e6f2913847e2d0915d9945c7649a68d487
Luiz Cezer <lccezinha@gmail.com>
2021-02-10 09:55:13 -0300

    Setup Absinthe and first query

---
+++ lib/community_web/router.ex
@@ -0,0 +1,32 @@
+defmodule CommunityWeb.Router do
+  use CommunityWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
+  end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+      live_dashboard "/dashboard", metrics: CommunityWeb.Telemetry
+    end
+  end
+end
